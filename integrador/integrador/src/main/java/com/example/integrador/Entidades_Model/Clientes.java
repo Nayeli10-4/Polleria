afb2445d83c246b9f4b6924e0088e9ce61062f8d
hp <hp@DESKTOP-DILREOG>
2024-10-20 15:09:15 -0500

    cambio de controlador

--- integrador/integrador/src/main/java/com/example/integrador/Entidades_Model/Clientes.java
+++ integrador/integrador/src/main/java/com/example/integrador/Entidades_Model/Clientes.java
@@ -4,7 +4,7 @@
  */
 package com.example.integrador.Entidades_Model;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,7 +22,7 @@ import lombok.NoArgsConstructor;
 @Table (name = "clientes")
 public class Clientes {
     @Id
-    @Column
+    
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long cliente_id;
     
